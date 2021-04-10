dc2ff52561b5c649fcf20cbb00bf8b1ecb8d02b5
Soldy <4786022+Soldy@users.noreply.github.com>
2021-04-10 23:22:27 +0100

    better example
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example.js
+++ example.js
@@ -1,9 +1,11 @@
 
 const boxrc = new (require('./index.js')).base();
+const $stdio = new (require('consolestdiorc')).base();
 
-
-boxrc.set('columns', 10);
-boxrc.set('rows', 5);
+$stdio.clear();
+$stdio.cursorHide();
+boxrc.set('columns', 25);
+boxrc.set('rows', 7);
 
 boxrc.add(
     'hugo the amazing cool'
@@ -22,3 +24,9 @@ boxrc.add(
     'not a rat'
 );
 boxrc.print();
+
+
+setTimeout(()=>{
+    $stdio.cursorShow();
+    $stdio.printTo('',0,0);
+},5000);

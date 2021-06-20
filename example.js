3658c556ed7ff0a8a40e56806cd4cc743f7d3844
Soldy <4786022+Soldy@users.noreply.github.com>
2021-06-20 20:37:43 +0100

    example extend
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example.js
+++ example.js
@@ -1,32 +1,43 @@
 
-const boxrc = new (require('./index.js')).base();
+const $boxrc = new (require('./index.js')).base();
 const $stdio = new (require('consolestdiorc')).base();
 
+let x = 4;
+let y = 4;
 $stdio.clear();
 $stdio.cursorHide();
-boxrc.set('columns', 25);
-boxrc.set('rows', 7);
+$boxrc.set('columns', 25);
+$boxrc.set('rows', 7);
+$boxrc.move(x,y);
 
-boxrc.add(
+$boxrc.add(
     'hugo the amazing cool'
 );
 
 
-boxrc.add(
+$boxrc.add(
     'hugo a dog'
 );
 
-boxrc.add(
+$boxrc.add(
     'not a cat'
 );
 
-boxrc.add(
+$boxrc.add(
     'not a rat'
 );
-boxrc.print();
+$boxrc.print();
 
+setInterval(()=>{
+    x++;
+    y++;
+    $boxrc.clean();
+    $boxrc.move(x,y);
+    $boxrc.print();
+},500);
 
 setTimeout(()=>{
     $stdio.cursorShow();
     $stdio.printTo('',0,0);
+    process.exit();
 },5000);
